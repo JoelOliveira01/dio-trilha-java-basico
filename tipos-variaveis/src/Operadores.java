d7a92050331b7125d2dd81927fe2bcb07b34c3b9
Joel Oliveira <joeljoseromero7@gmail.com>
2024-05-17 21:57:48 -0300

    Atualização da pasta Tipos de variaveis

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+
+    public static void main(String[]args){
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado ="";
+
+        if (a==b)
+            resultado = "Verdadeiro";
+        else
+            resultado = "Falso";
+    }
+ }
+
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Joel José Novais Oliveira";
+
+        System.out.print(meuNome);
+
+        double salarioMinimo = 2500;
+            System.out.print(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroNormal2 = (short) numeroNormal;
+            System.out.print(numeroNormal2);
+
+        int numero = 5;
+          System.out.print(numero);
     }
 }
