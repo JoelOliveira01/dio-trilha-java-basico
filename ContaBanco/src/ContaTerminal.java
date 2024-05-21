1c561dc812ec0cb7b249cdac579877f93b663fc7
Joel Oliveira <joeljoseromero7@gmail.com>
2024-05-21 07:44:30 -0300

    .

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,12 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args){
         
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
 
+            System.out.println("Por favor, insira o número da conta : ");
+            int numero = scanner.nextInt();
+
+            System.out.println("Por favor, insira o número da agência : ");
+            String agencia = scanner.next();
+
+            System.out.println("Inserir nome : ");
+            String nomeCliente = scanner.next();
+
+            System.out.println("Insira seu saldo : ");
+            double saldo = scanner.nextDouble();
+
+
+            System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é : " + agencia + ", conta : " + numero + ", e seu saldo : R$ " + saldo + "já está disponivel para saque.");
+
+           
+        }
 
 
 
 
         
-        System.out.println("Hello, World!");
     }
 }
--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 public class AboutMe {
 
     public static void main(String[]args){
-        //criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (//criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite seu nome : ");
+            String nome = scanner.next();
 
-        System.out.println("Digite seu nome : ");
-        String nome = scanner.next();
+            System.out.println("Digite seu sobrenome : ");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite seu sobrenome : ");
-        String sobrenome = scanner.next();
+            System.out.println("Digite sua idade : ");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite sua idade : ");
-        int idade = scanner.nextInt();
-
-        System.out.println("Digite sua altura : ");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite sua altura : ");
+            double altura = scanner.nextDouble();
 
 
-    
-        /* 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.parseInt(args[2]);
-        double altura = Double.parseDouble(args[3]);*/
+   
+            /* 
+            String nome = args [0];
+            String sobrenome = args [1];
+            int idade = Integer.parseInt(args[2]);
+            double altura = Double.parseDouble(args[3]);*/
 
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+        }
     }
 }
